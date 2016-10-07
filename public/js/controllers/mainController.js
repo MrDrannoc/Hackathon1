03202fad2bfc16576fddb6c764e9eadb4f144540
MrDrannoc <poyer.k@gmail.com>
2016-10-07 07:23:29 +0200

    je sais plus ce que j'ai fait

--- public/js/controllers/mainController.js
+++ public/js/controllers/mainController.js
@@ -8,26 +8,26 @@ class mainController {
     load() {
         this.todoService.getAll().then((res) => {
             this.todos = res.data;
-        })
+        });
     }
 
     create() {
         this.todoService.create(this.todo).then(() => {
             this.todo = '';
-            this.load()
-        })
+            this.load();
+        });
     }
 
     update(todo) {
         this.todoService.update(todo._id, todo.description).then(() => {
-            this.load()
-        })
+            this.load();
+        });
     }
 
     delete(todo) {
         this.todoService.delete(todo._id).then(() => {
-            this.load()
-        })
+            this.load();
+        });
     }
     getHU(origin,dest){
       console.log(origin,dest);
